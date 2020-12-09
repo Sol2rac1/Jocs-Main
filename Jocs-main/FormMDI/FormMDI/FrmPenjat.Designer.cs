02c21c30ee2c22c2791007240aa6e9034e1e20b1
Sol2rac1 <57619745+Sol2rac1@users.noreply.github.com>
2020-12-09 09:00:36 +0100

    Colgado

--- Jocs-main/FormMDI/FormMDI/FrmPenjat.Designer.cs
+++ Jocs-main/FormMDI/FormMDI/FrmPenjat.Designer.cs
@@ -28,7 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tb1 = new System.Windows.Forms.TextBox();
+            this.bt1 = new System.Windows.Forms.Button();
+            this.incog0 = new System.Windows.Forms.Label();
+            this.incog1 = new System.Windows.Forms.Label();
+            this.incog2 = new System.Windows.Forms.Label();
+            this.incog3 = new System.Windows.Forms.Label();
+            this.incog4 = new System.Windows.Forms.Label();
+            this.imgColgado = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imgColgado)).BeginInit();
             this.SuspendLayout();
+            // 
+            // tb1
+            // 
+            this.tb1.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.tb1.Location = new System.Drawing.Point(172, 509);
+            this.tb1.MaxLength = 5;
+            this.tb1.Name = "tb1";
+            this.tb1.Size = new System.Drawing.Size(163, 30);
+            this.tb1.TabIndex = 5;
+            // 
+            // bt1
+            // 
+            this.bt1.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.bt1.Location = new System.Drawing.Point(195, 545);
+            this.bt1.Name = "bt1";
+            this.bt1.Size = new System.Drawing.Size(109, 39);
+            this.bt1.TabIndex = 8;
+            this.bt1.Text = "Comprobar";
+            this.bt1.UseVisualStyleBackColor = true;
+            this.bt1.Click += new System.EventHandler(this.bt1_Click);
+            // 
+            // incog0
+            // 
+            this.incog0.AutoSize = true;
+            this.incog0.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.incog0.Location = new System.Drawing.Point(196, 429);
+            this.incog0.Name = "incog0";
+            this.incog0.Size = new System.Drawing.Size(20, 28);
+            this.incog0.TabIndex = 9;
+            this.incog0.Text = "-";
+            // 
+            // incog1
+            // 
+            this.incog1.AutoSize = true;
+            this.incog1.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.incog1.Location = new System.Drawing.Point(222, 429);
+            this.incog1.Name = "incog1";
+            this.incog1.Size = new System.Drawing.Size(20, 28);
+            this.incog1.TabIndex = 10;
+            this.incog1.Text = "-";
+            // 
+            // incog2
+            // 
+            this.incog2.AutoSize = true;
+            this.incog2.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.incog2.Location = new System.Drawing.Point(248, 429);
+            this.incog2.Name = "incog2";
+            this.incog2.Size = new System.Drawing.Size(20, 28);
+            this.incog2.TabIndex = 11;
+            this.incog2.Text = "-";
+            // 
+            // incog3
+            // 
+            this.incog3.AutoSize = true;
+            this.incog3.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.incog3.Location = new System.Drawing.Point(274, 429);
+            this.incog3.Name = "incog3";
+            this.incog3.Size = new System.Drawing.Size(20, 28);
+            this.incog3.TabIndex = 12;
+            this.incog3.Text = "-";
+            // 
+            // incog4
+            // 
+            this.incog4.AutoSize = true;
+            this.incog4.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.incog4.Location = new System.Drawing.Point(300, 429);
+            this.incog4.Name = "incog4";
+            this.incog4.Size = new System.Drawing.Size(20, 28);
+            this.incog4.TabIndex = 13;
+            this.incog4.Text = "-";
+            // 
+            // imgColgado
+            // 
+            this.imgColgado.BackgroundImage = global::FormMDI.Properties.Resources.Intento1;
+            this.imgColgado.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.imgColgado.Location = new System.Drawing.Point(122, 27);
+            this.imgColgado.Name = "imgColgado";
+            this.imgColgado.Size = new System.Drawing.Size(287, 323);
+            this.imgColgado.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.imgColgado.TabIndex = 14;
+            this.imgColgado.TabStop = false;
             // 
             // FrmPenjat
             // 
@@ -36,13 +126,33 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Khaki;
             this.ClientSize = new System.Drawing.Size(552, 601);
+            this.Controls.Add(this.imgColgado);
+            this.Controls.Add(this.incog4);
+            this.Controls.Add(this.incog3);
+            this.Controls.Add(this.incog2);
+            this.Controls.Add(this.incog1);
+            this.Controls.Add(this.incog0);
+            this.Controls.Add(this.bt1);
+            this.Controls.Add(this.tb1);
             this.Name = "FrmPenjat";
             this.Text = "El Penjat";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FrmPenjat_FormClosing);
+            this.Load += new System.EventHandler(this.FrmPenjat_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imgColgado)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tb1;
+        private System.Windows.Forms.Button bt1;
+        private System.Windows.Forms.Label incog0;
+        private System.Windows.Forms.Label incog1;
+        private System.Windows.Forms.Label incog2;
+        private System.Windows.Forms.Label incog3;
+        private System.Windows.Forms.Label incog4;
+        private System.Windows.Forms.PictureBox imgColgado;
     }
 }
--- Jocs-main/FormMDI/FormMDI/FrmPenjat.cs
+++ Jocs-main/FormMDI/FormMDI/FrmPenjat.cs
@@ -12,15 +12,130 @@ namespace FormMDI
 {
     public partial class FrmPenjat : Form
     {
+        //variables
+        string Solucion;
+        int resuelto = 0;
+        string escrito;
+        int intentos = 1;
+        Random rnd = new Random();
+        int r = 0;
+
         public event EventHandler emTanquen;
         public FrmPenjat()
         {
             InitializeComponent();
+            seleccionarPlabra();
+        }
+
+        private void seleccionarPlabra()
+        {
+            r = rnd.Next(1, 11);
+            switch (r)
+            {
+                case 1: Solucion = "abajo"; break;
+                case 2: Solucion = "angel"; break;
+                case 3: Solucion = "abajo"; break;
+                case 4: Solucion = "aguja"; break;
+                case 5: Solucion = "niños"; break;
+                case 6: Solucion = "barco"; break;
+                case 7: Solucion = "ebrio"; break;
+                case 8: Solucion = "oasis"; break;
+                case 9: Solucion = "vacio"; break;
+                case 10: Solucion = "tapon"; break;
+                case 11: Solucion = "jaume";break;
+            }
         }
 
         private void FrmPenjat_FormClosing(object sender, FormClosingEventArgs e)
         {
             emTanquen(this, EventArgs.Empty);
+        }
+
+        private void FrmPenjat_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void estadoPalo()
+        {
+            //CONTADOR DE FALLOS Y CAMBIO DE IMAGEN
+            intentos = intentos + 1;
+            switch (intentos)
+            {
+                case 2: imgColgado.BackgroundImage = (Properties.Resources.Intento2); break;
+                case 3: imgColgado.BackgroundImage = (Properties.Resources.Intento3); break;
+                case 4: imgColgado.BackgroundImage = (Properties.Resources.Intento4); break;
+                case 5: imgColgado.BackgroundImage = (Properties.Resources.Intento5); break;
+                case 6: imgColgado.BackgroundImage = (Properties.Resources.Intento6); break;
+                case 7: imgColgado.BackgroundImage = (Properties.Resources.Intento7); break;
+                case 8: imgColgado.BackgroundImage = (Properties.Resources.Intento8); break;
+                case 9: mostrarDerrota(); break;
+            }
+        }
+
+        private void mostrarDerrota()
+        {
+            //muestra solucion y la respuesta
+            MessageBox.Show("Que pena has perdido", "Perdedor", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            incog0.Text = Solucion[0].ToString();
+            incog1.Text = Solucion[1].ToString();
+            incog2.Text = Solucion[2].ToString();
+            incog3.Text = Solucion[3].ToString();
+            incog4.Text = Solucion[4].ToString();
+        }
+
+        private void bt1_Click(object sender, EventArgs e)
+        {
+            escrito = tb1.Text.ToString();
+            if (escrito.Length == 1)
+            {
+                if (Solucion.Contains(escrito) == true)
+                {
+                    ponerLetra();
+                }
+                else estadoPalo();
+
+            }
+            else if (escrito == Solucion)
+            {
+                MessageBox.Show("Felicidades has ganado", "Ganador", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                incog0.Text = Solucion[0].ToString();
+                incog1.Text = Solucion[1].ToString();
+                incog2.Text = Solucion[2].ToString();
+                incog3.Text = Solucion[3].ToString();
+                incog4.Text = Solucion[4].ToString();
+            }
+            else
+            {
+                estadoPalo();
+            }
+            tb1.Text = "";
+
+        }
+
+        private void ponerLetra()
+        {
+            int i;
+            for (i = 0; i < 5; i++)
+            {
+                if (escrito[0] == Solucion[i])
+                {
+                    switch (i)
+                    {
+                        case 0: incog0.Text = escrito; resuelto++; break;
+                        case 1: incog1.Text = escrito; resuelto++; break;
+                        case 2: incog2.Text = escrito; resuelto++; break;
+                        case 3: incog3.Text = escrito; resuelto++; break;
+                        case 4: incog4.Text = escrito; resuelto++; break;
+                    }
+
+                }
+            }
+
+            if (resuelto == 5)
+            {
+                MessageBox.Show("Felicidades has ganado", "Ganador", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
         }
     }
 }
